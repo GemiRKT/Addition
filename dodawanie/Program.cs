5f5c53d762d73dda9f33fee0a974c1741d6edd27
GemiRKT <gemirkt@gmail.com>
2019-01-17 20:53:04 +0000

    ok

--- dodawanie/Program.cs
+++ dodawanie/Program.cs
@@ -10,118 +10,157 @@ namespace dodawanie
     {
         static void Main(string[] args)
         {
-
-            int FirstNumber = 0;
-            int SecondNumber = 0;
-            int MaxNumber = 10;
-            int LowNumber = 0;
-            int Sum;
+            int FirstNum;
+            int SecondNum;           
+            int Num = 0;
+            int wybor = 0;
+            string wyborstring = "";
+            string exit = "";
             
-           
-            
-            Console.WriteLine("Witamy w dodwaniu");
 
+            Console.WriteLine("Witamy w dzialaniach");
+            Console.WriteLine("Podaj Pierwsza Liczbe : ");
+            FirstNum = PobierzLiczbe(Num);
             do
             {
                 try
                 {
-                    Console.WriteLine("Podaj Pierwsza Liczbe 0-10: ");
-                    int GetNumber = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Wybierz operacje wpisujac liczbe: ");
+                    Console.WriteLine("1.Dodawanie");
+                    Console.WriteLine("2. Odejmowanie");
+                    wyborstring = Console.ReadLine();
+                    wybor = int.Parse(wyborstring);
 
-                    if (GetNumber <= MaxNumber && GetNumber > LowNumber)
+                    switch (wybor)
                     {
-                        
-                        break;
 
+                        case 1:
+                            {
+                                Console.WriteLine("Podaj Druga Liczbe : ");
+                                SecondNum = PobierzLiczbe(Num);
+                                Dzialania wynik = new Dzialania();
+                                wynik.Dodawanie(FirstNum, SecondNum);
+                                exit = "exit";
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Podaj Druga Liczbe : ");
+                                SecondNum = PobierzLiczbe(Num);
+                                Dzialania wynik = new Dzialania();
+                                wynik.Odejmowanie(FirstNum, SecondNum);
+                                exit = "exit";
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Podano bledny znak/liczbe wybierz ponownie 1 lub 2.");
+                                break;
+                            }
                     }
-                    else
-                        FirstNumber = GetNumber;
-                    Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
-
-
-
                 }
+
                 catch (FormatException fEx)
                 {
                     Console.WriteLine(fEx.Message);
-
-
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
                 }
                 catch (OverflowException OverEx)
                 {
                     Console.WriteLine(OverEx.Message);
-
-
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
                 }
                 catch (ArithmeticException ArgEx)
                 {
                     Console.WriteLine(ArgEx.Message);
-
-
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
                 }
                 catch (Exception Ex)
                 {
                     Console.WriteLine("Coœ posz³o nie tak");
-
-
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
                 }
-            } while (true);
+        } while (exit != "exit");
 
+        }
+        static int PobierzLiczbe(int Num)
+        {
+
+            String Number = "";
+            String con = "";
 
             do
             {
-                try
-                {
-                    Console.WriteLine("Podaj druga Liczbe 0-10: ");
-                    int GetNumber = int.Parse(Console.ReadLine());
-
-
-                    if (GetNumber <= MaxNumber && GetNumber > LowNumber)
-
+                    try
                     {
-                        break;
 
+                        Number = Console.ReadLine();
+                        Num = int.Parse(Number);
+                        con = "wkoncu";
                     }
-                    else
-                        SecondNumber = GetNumber;
-                    Console.WriteLine("nie Poprawana liczba sprobuj ponownie");
+                    catch (FormatException fEx)
+                    {
+                        Console.WriteLine(fEx.Message);
+                        Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                    }
+                    catch (OverflowException OverEx)
+                    {
+                        Console.WriteLine(OverEx.Message);
+                        Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                    }
+                    catch (ArithmeticException ArgEx)
+                    {
+                        Console.WriteLine(ArgEx.Message);
+                        Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                    }
+                    catch (Exception Ex)
+                    {
+                        Console.WriteLine("Coœ posz³o nie tak");
+                        Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                    }
+            } while (con != "wkoncu");
+            return Num;
+        }
 
+        
 
-                }
-                catch (FormatException fEx)
-                {
-                    Console.WriteLine(fEx.Message);
-
-                }
-                catch (OverflowException OverEx)
-                {
-                    Console.WriteLine(OverEx.Message);
-
-                }
-                catch (ArithmeticException ArgEx)
-                {
-                    Console.WriteLine(ArgEx.Message);
-
-                }
-                catch (Exception Ex)
-                {
-                    
-                    Console.WriteLine("Coœ posz³o nie tak");
-                }
+        class Dzialania
+        {
+                         
+            public int Dodawanie(int FirstNum ,int SecondNum)
+            {
+                int wynik;
+                wynik = FirstNum + SecondNum;
+                Console.WriteLine("Wynik Dodawania to : " + wynik + "");
+                Console.WriteLine("Nacisnij dowolny klawisz aby wyjsc");
+                Console.ReadKey();
                 
+                return wynik;
 
             }
-            while (true);
-
-
-
-            Sum = FirstNumber + SecondNumber;
-                Console.WriteLine("Wynik dodawania to : " + Sum + "");
+            public int Odejmowanie(int FirstNum, int SecondNum)
+            {
+                int wynik;
+                wynik = FirstNum - SecondNum;
+                Console.WriteLine("Wynik Odejmowania to : " + wynik + "");
+                Console.WriteLine("Nacisnij dowolny klawisz aby wyjsc");
                 Console.ReadKey();
-            
+                
+                return wynik;
 
-
-
+            }
         }
     }
 }
+
+
+
+
+
+        
+
+
+
+
+
+
