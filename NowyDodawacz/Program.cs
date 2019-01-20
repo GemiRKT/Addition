15abf02e43cd64aab5a2df47dbb9af97daa59d91
GemiRKT <gemirkt@gmail.com>
2019-01-20 11:32:11 +0000

    ok

---
+++ NowyDodawacz/Program.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NowyDodawacz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 0;
+
+            String Operator = "";
+            Console.WriteLine("Witamy w SuperNowym Liczydle 2019");
+
+            Pobrania number = new Pobrania();
+            
+            int FirstNum = number.Sprawdz(Num);
+ 
+            Pobrania Op = new Pobrania();
+            String Oper =Op.PobierzOperator(Operator);
+
+            int SecondNum = number.Sprawdz(Num);
+
+            Wybor end = new Wybor();
+            end.Wybordzialania(FirstNum, SecondNum, Oper);
+            Console.ReadKey();
+        }
+    }
+
+
+    class Dzialania
+    {
+        int Wynik;
+        
+
+        public int Dodawanie(int FirstNum, int SecondNum)
+        {
+            Wynik =FirstNum + SecondNum;
+            Console.WriteLine("Wynik dodawania wynosi : " + Wynik + "");
+            return Wynik;
+        }
+        public int Odejmowanie(int FirstNum, int SecondNum)
+        {
+            Wynik = FirstNum - SecondNum;
+            Console.WriteLine("Wynik odejmowania wynosi : " + Wynik + "");
+            return Wynik;
+        }
+        public int Mnozenie(int FirstNum, int SecondNum)
+        {
+            Wynik = FirstNum * SecondNum;
+            Console.WriteLine("Wynik mnozenia wynosi : " + Wynik + "");
+            return Wynik;
+        }
+        public int Dzielenie(int FirstNum, int SecondNum)
+        {
+            try
+            {
+                Wynik = FirstNum / SecondNum;    
+                Console.WriteLine("Wynik dzielenia wynosi : " + Wynik + "");
+                return Wynik;
+            }
+            catch (DivideByZeroException)
+            { };
+            return Wynik;
+            
+        }
+
+
+        
+    }
+    class Wybor : Dzialania
+    {
+        public void Wybordzialania(int FirstNum, int SecondNum, string Operator)
+        {
+
+
+            switch (Operator)
+            {
+                case "+": { Dodawanie(FirstNum, SecondNum); break; }
+                case "-": { Odejmowanie(FirstNum, SecondNum); break; }
+                case "*": { Mnozenie(FirstNum, SecondNum); break; }
+                case "/": { Dzielenie(FirstNum, SecondNum); break; }
+
+            }
+        }
+    }
+
+
+    class Pobrania
+    {
+        public string Number = "";
+
+
+
+        public int TakeNumber(int Num)
+        {
+            Console.WriteLine("Prosze wpisac liczbe: ");
+            Number = Console.ReadLine();
+            Num = int.Parse(Number);
+            return Num;
+        }
+        public string PobierzOperator(string Operator)
+        {
+            string[] Operatory = { "+", "-", "*", "/" };
+            string exit = "";
+            Console.WriteLine("Prosze nacisnac Operatora Dzialania: ");
+            Console.WriteLine("-");
+            Console.WriteLine("+");
+            Console.WriteLine("*");
+            Console.WriteLine("/");
+            do
+            {
+                Operator = Console.ReadLine();
+                if (Operatory.Contains(Operator))
+                {
+                    exit = "exit";
+                    return Operator;
+                }
+                else
+                {
+                    Console.WriteLine("Niepoprawny wybor, wybierz +,- lub * ");
+                }
+            } while (exit != "exit");
+            return Operator;
+        }
+
+
+    }
+    class Sprawdz:Pobrania
+    {
+        public int SprawdzLiczby(int Num)
+        {
+            string end = "";
+            
+           do
+            {
+                try
+                {
+                   Num =  TakeNumber(Num);
+                   
+                    end = "end";
+
+
+                }
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine(fEx.Message);
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                }
+                catch (OverflowException OverEx)
+                {
+                    Console.WriteLine(OverEx.Message);
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                }
+                catch (ArithmeticException ArgEx)
+                {
+                    Console.WriteLine(ArgEx.Message);
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine("Coś poszło nie tak");
+                    Console.WriteLine("Wpisz Liczbe Ponownie: ");
+                }
+            } while (end != "end");
+            return Num;
+            
+        }
+        
+    }
+   
+
+}
+
--- NowyDodawacz/Properties/AssemblyInfo.cs
+++ NowyDodawacz/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dodawanie")]
+[assembly: AssemblyTitle("NowyDodawacz")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dodawanie")]
+[assembly: AssemblyProduct("NowyDodawacz")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("91563d00-ff86-4608-807b-d7eed3c65ca7")]
+[assembly: Guid("6a1c5576-00bc-444b-a63a-e813b8efc45f")]
 
 // Version information for an assembly consists of the following four values:
 //
